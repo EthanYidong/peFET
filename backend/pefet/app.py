96f00d24051d479d09f9d405c17cc866d23e5f0a
EthanYidong <ethanyidong@gmail.com>
2022-05-29 01:17:09 -0600

    Tweaked hosting settings to work better on MacOS - Port 5000 is in use by some system program

--- backend/pefet/app.py
+++ backend/pefet/app.py
@@ -1,8 +1,19 @@
+import logging
+
 from flask import Flask
+from flask_cors import CORS
 
 from peewee import SqliteDatabase
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+# To enable logging for flask-cors,
+logging.getLogger('flask_cors').level = logging.DEBUG
+
+# TODO: add stricter CORS restrictions
+CORS(app, send_wildcard=True)
 app.config.from_object('config.Configuration')
 
 db = SqliteDatabase(app.config['DATABASE'])
