e2b1da95bec11773902099f7c34de26abe126ef1
EthanYidong <ethanyidong@gmail.com>
2022-05-29 01:25:35 -0600

    Added home button

--- frontend/src/pages/create.tsx
+++ frontend/src/pages/create.tsx
@@ -1,13 +1,13 @@
 import { createResource, For } from 'solid-js';
 import { createStore } from 'solid-js/store';
-import { useNavigate } from 'solid-app-router';
+import { useNavigate, Link } from 'solid-app-router';
 
 import ParticipantEditor from '@/components/participant-editor';
 import type { Participant } from '@/lib/models/participant';
 import { API_URL, useToken } from '@/lib/api';
 
 export default function Create() {
-  const [token, setToken, clearToken] = useToken(); 
+  const [token, _setToken, clearToken] = useToken(); 
   const [participants, setParticipants] = createStore<Participant[]>([]);
   const navigate = useNavigate();
 
@@ -39,7 +39,8 @@ export default function Create() {
   createResource(validateToken);
 
   return (
-    <div class="flex flex-col p-8">
+    <div class="flex flex-col p-8 gap-2">
+      <div><Link class="bg-blue-600 text-white p-2 rounded-md" href='/'>&lt; Home</Link></div>
       <h1 class="text-2xl">Create New Event Listing</h1>
       <h2 class="text-xl">If you're seeing this, that means you have successfully logged in. This page does not affect anything. It is only to demonstrate that the user is logged in, and serve as a prototype for event creation.</h2>
       <div>
