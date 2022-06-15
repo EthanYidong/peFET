b5cf1ce9262f7311b86bdb5f67f98de0170971fc
Leeisateam <leeisateam80@gmail.com>
2022-06-15 23:47:30 +0800

    Added User, Event and Participant models, and a json page.
    Added User, Event and Participant models in backend/pefet/api/models.py. Added a json page in backend/pefet/views/py.

--- backend/pefet/api/models.py
+++ backend/pefet/api/models.py
@@ -1,3 +1,25 @@
+from enum import unique
+import json
 from django.db import models
+from django.http import JsonResponse
 
-# Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=100, default="AnonymousUser")
+  email = models.CharField(max_length=100, unique=True, primary_key=True)
+  password = models.BinaryField(max_length=60)
+  def __str__(self):
+    return self.name
+
+class Event(models.Model):
+  name = models.CharField(max_length=100)
+  date = models.DateTimeField()
+  creator = models.ForeignKey(User, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+class Participant(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.CharField(max_length=100)
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
